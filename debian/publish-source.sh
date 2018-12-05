43a0e120f482500aea624b21f0b5389af538b219
Yadickson Soto <yadickson.soto@basedos.cl>
2018-12-05 14:45:41 -0300

    Update build dependencies

--- debian/publish-source.sh
+++ debian/publish-source.sh
@@ -9,7 +9,7 @@ ORIG_TARBALL="../${PKG}_${VERSION}.orig.tar.xz"
 [ -f "${ORIG_TARBALL}" ] || exit 1
 
 rm -rf .pc
-rm -rf pom.xml src
+rm -rf pom.xml* src
 
 tar --strip-components=1 -xJf "${ORIG_TARBALL}" "${PKG}-${VERSION}" || exit 1
 
