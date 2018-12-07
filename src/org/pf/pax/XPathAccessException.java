482a5ec1bdbfd8e6e2e0b077bb80233550771d44
Yadickson Soto <yadickson.soto@basedos.cl>
2018-12-07 12:13:26 -0300

    Add source code

---
+++ src/org/pf/pax/XPathAccessException.java
@@ -0,0 +1,52 @@
+// ===========================================================================
+// CONTENT  : CLASS XPathAccessException
+// AUTHOR   : Manfred Duchrow
+// VERSION  : 1.0 - 14/01/2012
+// HISTORY  :
+//  14/01/2012  mdu  CREATED
+//
+// Copyright (c) 2012, by MDCS. All rights reserved.
+// ===========================================================================
+package org.pf.pax ;
+
+// ===========================================================================
+// IMPORTS
+// ===========================================================================
+import java.lang.RuntimeException ;
+
+/**
+ * A runtime exception that encapsulate all exceptions that might occur
+ * in accessing XML data via XPath queries.
+ *
+ * @author Manfred Duchrow
+ * @version 1.0
+ */
+public class XPathAccessException extends RuntimeException
+{
+  /**
+   * Initialize the new instance with default values.
+   */
+  public XPathAccessException()
+  {
+    super() ;
+  } // XPathAccessException()
+
+	public XPathAccessException(String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XPathAccessException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XPathAccessException(Throwable cause)
+	{
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+    
+} // class XPathAccessException
